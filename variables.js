5dc4700dc9a7f23b72b794d40c6a4a5c812ba5c7
Efrelle Joyce Ducyao <efrellejoyced@gmail.com>
2024-09-10 12:14:59 +0700

    completed variables section

---
+++ variables.js
@@ -0,0 +1,10 @@
+console.log("****VARIABLES****");
+let name = "Efrelle Joyce C. Ducyao";
+let whatDoYouWannaBecomeInYourLife = "Programmer"
+let gender = "Female";
+let twitterHandle = "EDucyao61563";
+
+console.log("Name", name);
+console.log("What do you want to become in your life?", whatDoWannaBecomeInYourLife);
+console.log("Gender:", gender);
+console.log("Twitter Handle:", twitterHandle);
